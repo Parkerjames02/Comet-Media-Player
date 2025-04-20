84c4cd7f9df92107d2ea67af0b67c830641fb922
Parkerjames02 <87055376+Parkerjames02@users.noreply.github.com>
2025-04-19 20:37:51 -0700

    Add project files.

---
+++ Perfect Ambience/FormMain.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Perfect_Ambience
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.FileList = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(924, 960);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(85, 33);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "&Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // FileList
+            // 
+            this.FileList.FormattingEnabled = true;
+            this.FileList.Location = new System.Drawing.Point(849, 911);
+            this.FileList.Name = "FileList";
+            this.FileList.Size = new System.Drawing.Size(237, 43);
+            this.FileList.TabIndex = 2;
+            this.FileList.SelectedIndexChanged += new System.EventHandler(this.FileList_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer
+            // 
+            this.axWindowsMediaPlayer.Enabled = true;
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(1896, 884);
+            this.axWindowsMediaPlayer.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Perfect_Ambience.Properties.Resources.Background;
+            this.ClientSize = new System.Drawing.Size(1904, 1001);
+            this.Controls.Add(this.FileList);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.axWindowsMediaPlayer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Comet Media Player";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListBox FileList;
+    }
+}
+
---
+++ Perfect Ambience/FormMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Perfect_Ambience
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Multiselect=true,ValidateNames=true,Filter= "WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MP4|*.mp4|MKV|*.mkv" }) 
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<MediaFile> files = new List<MediaFile>();
+                    foreach(string fileName in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(fileName);
+                        files.Add(new MediaFile() { FileName = Path.GetFileNameWithoutExtension(fi.FullName),Path=fi.FullName});
+                    }
+                    FileList.DataSource = files;
+                    FileList.ValueMember = "Path";
+                    FileList.DisplayMember = "FileName";
+                }
+            }
+        }
+
+        private void FileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = FileList.SelectedItem as MediaFile;
+            if(file != null)
+            {
+                axWindowsMediaPlayer.URL = file.Path;
+                axWindowsMediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            FileList.ValueMember = "Path";
+            FileList.DisplayMember = "FileName";
+        }
+    }
+}
